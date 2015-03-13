b38841545a6d855a61a723c5acc00345a77af862
Matt Pill <matt@hellofutu.re>
2015-03-13 15:05:19 +0000

    First commit - ready for feedback

---
+++ js/init.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($){
+	var smallWindow = false;
+
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$('#logo-image').attr('src', 'img/logo2.png')
+			$(".important-class").addClass("padding-on-my-header");
+		}
+		if (scroll < 50) {
+			$(".important-class").removeClass("padding-on-my-header");
+			$('#logo-image').attr('src', 'img/logo.png')
+		}
+	}).resize(function(){
+		if ( !smallWindow && this.innerWidth <= 1024 ) {
+			smallWindow = true;
+			$('.top-bar-section').find('ul.right').hide(0).delay(500).show(0);
+		}
+		if ( smallWindow && this.innerWidth > 1024 ) {
+			smallWindow = false;
+		}
+	});
+});
+
